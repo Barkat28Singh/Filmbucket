98c33d3110686230c34fdb68cdb0dc026fef5af4
Barkat28Singh <Barkat1213singh@gmail.com>
2023-04-19 13:58:48 -0400

    Implemented realtime database in trivia page and sign up form page

--- Controller/trivia.js
+++ Controller/trivia.js
@@ -15,7 +15,48 @@ $("#icon-menu").click(function () {
   }
 });
 
-$("#submitbtn").click(function () {
-  window.location.href = "/views/trivia-thanks.html";
-  return false;
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCYdPisfkCcLxPuzy-jM2UQRvORxPq5olk",
+  authDomain: "film-bucket-v3.firebaseapp.com",
+  databaseURL: "https://film-bucket-v3-default-rtdb.firebaseio.com",
+  projectId: "film-bucket-v3",
+  storageBucket: "film-bucket-v3.appspot.com",
+  messagingSenderId: "717415814149",
+  appId: "1:717415814149:web:992815ffeae3bca17e386c",
 });
+
+var TriviaFormDB = firebase.database().ref("TriviaForm"); //Creating db location reference
+
+//When I submit the form, it will call this function the submitForm function
+
+function submitForm(event) {
+  //e will be using to reference the event object
+  event.preventDefault(); //prevent the form from going to another page when we submit
+
+  //Get ref for fields inside the form
+  var ans1 = document.getElementById("ans1").value;
+  var ans2 = document.getElementById("ans2").value;
+  var ans3 = document.getElementById("ans3").value;
+
+  saveData(ans1, ans2, ans3);
+  console.log(ans1, ans2, ans3);
+  document.getElementById("TriviaForm").reset();
+}
+
+const saveData = (ans1, ans2, ans3) => {
+  //Push this array onto our database
+  var newSignForm = TriviaFormDB.push();
+
+  //Set the values of the array
+  //to name, email, and message content
+  newSignForm.set({
+    ans1: ans1,
+    ans2: ans2,
+    ans3: ans3,
+  });
+};
+setTimeout(() => {
+  $("#submitbtn").click(function () {
+    window.location.href = "/views/trivia-thanks.html";
+  });
+}, 3000);
